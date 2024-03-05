0c501f8164828ec4ca38274829620b8098340aa8
Matei C <matei23@yahoo.com>
2024-03-05 14:54:49 +0200

    finalul

--- lab2.c
+++ lab2.c
@@ -2,19 +2,17 @@
 #include <stdio.h>
 
 int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+	if(x>0)return function(x-2);
 }
 
 int main(){
 	int a=30, i=0;
-	
+
 	while (a>0){
 		a=function(a);
 		i++;
 	}
 	printf("%d", i);
-	return 0;	
+	return 0;
 }
 
